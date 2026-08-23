2b022502e562701a0ffa6d52c0d7dda7b704261d
LockVision <hassanelmaayati@gmail.com>
2026-08-23 10:27:32 +0300

    Add Animal model and fixed module.exports typo

---
+++ models/animal.js
@@ -0,0 +1,41 @@
+const mongoose= require('mongoose')
+
+const animalSchema= new mongoose.Schema({
+
+  name:{
+    type: String,
+    required: true,
+  }
+
+  ,species:{
+    type: String,
+    required:true,
+  }
+  ,breed:{
+    type: String,
+  }
+
+  ,birthdate:{
+    type: Date,
+  }
+
+  ,status:{
+    type: String,
+    enum:['Healthy','Sick','Sold','Deceased','Other'],
+    default:'Healthy'
+  }
+
+  ,notes:{
+    type: String,
+  }
+
+  ,Structure:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Structure',
+    required:true,
+  },
+
+});
+
+const Animal= mongoose.model('Animal',animalSchema)
+module.exports= Animal
