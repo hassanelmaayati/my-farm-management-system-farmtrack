8e3df7d3899d9329403fbfc7ae1415b9afb1a042
LockVision <hassanelmaayati@gmail.com>
2026-08-23 11:17:36 +0300

    typo and errors fixing

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,38 +25,38 @@ const signup= async(req,res)=>{
   }
 }
 
-
-const login=async(req,res)=>{
-
-  try{
-    const user=await User.findOne({username:req.body.username})
-    if(!user){
+const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    if (!user) {
       return res.send("Login failed, Try again.")
     }
-    const isPasswordCorrect=await bcrypt.compare(req.body.password, user.password)
-    if (!isPasswordCorrect){
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+    if (!isPasswordCorrect) {
       return res.send('Login failed, Try again.')
     }
 
-    req.session.user={username: user.username. _id: user._id},
-    req session.save(()=>{
+    req.session.user = { username: user.username, _id: user._id }
+    req.session.save(() => {
       res.redirect('/structures')
     })
-    
-  }catch(err){
+
+  } catch (err) {
     console.log(err)
     res.redirect('/')
   }
+} 
+
+const logout = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/')
+  })
+}
 
 const logout= (req,res)=>{
   req.session.destroy(()=>{
     res.redirect('/')
   })
 }
-  }
-}
-
-
-
 
 module.exports={ signup, login, logout}
