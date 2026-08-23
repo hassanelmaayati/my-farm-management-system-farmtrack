ee6acfeb649a18251d3fd66821040e5ecf28401f
LockVision <hassanelmaayati@gmail.com>
2026-08-23 10:14:36 +0300

    added structure model

---
+++ models/structure.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+
+const structureSchema=new mongoose.Schema({
+  name:{
+    type:String,
+    required:true,
+  },
+  type:{
+    type:String,
+    enum:['Barn','Stable','Coop','Pen','Other'],
+    required:true
+  },
+  notes:{
+    type:String,
+  },
+  user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true,
+  },
+})
+
+const Structure=mongoose.model('Structure', structureSchema)
+
+model.exports= structure
