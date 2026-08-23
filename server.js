2a5d9a8baad20d0ac03871d083a26db13619bb7a
LockVision <hassanelmaayati@gmail.com>
2026-08-23 09:06:35 +0300

    Connect app to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
+const mongoose=require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB: ${mongoose.connection.name}`);
+});
 
 app.get('/', (req, res) => {
   res.send('FarmTrack is running!');
